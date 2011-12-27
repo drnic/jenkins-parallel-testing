f044d35d14b4e84ea8b4278ae235c2af23d9b913
Dr Nic Williams <drnicwilliams@gmail.com>
2011-12-27 15:10:02 -0800

    two slow tests

---
+++ test/unit/post_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  test "the truth" do
+    sleep(1)
+    assert true
+  end
+  test "the truth again" do
+    sleep(1)
+    assert true
+  end
+end
