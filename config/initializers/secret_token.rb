66a210973bbf1cfa0fef8a14c6302796e01e5306
Dr Nic Williams <drnicwilliams@gmail.com>
2011-12-27 14:48:42 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JenkinsParallelTesting::Application.config.secret_token = 'e4531afc80a7d8a3b502a65f43de9693d9a6234a5320a180ae183ebae11dfc7ac441fefab0087b2ee0ba51fc0ee9928cb814d4c3bb8a162c1c0ae25a73d9c049'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JenkinsParallelTesting::Application.config.session_store :cookie_store, key: '_jenkins-parallel-testing_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JenkinsParallelTesting::Application.config.session_store :active_record_store
